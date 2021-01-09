7155e919db6b86adb55157abc3df5a804f3b302b
guest_user <guest_user@DESKTOP-ERMAB4N>
2021-01-08 22:21:32 -0500

    First Commit

---
+++ src/Gitpackage/Test1.java
@@ -0,0 +1,22 @@
+package Gitpackage;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		
+		String hungry="yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("Feed the baby...");
+			
+		}
+		else
+			System.out.println("Make her play...");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
